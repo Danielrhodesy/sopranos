d410c5ed0c6857f6533c090db716026188585787
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-07-18 15:29:16 +1200

    WCSS for web header top part

--- src/components/WebHeader.jsx
+++ src/components/WebHeader.jsx
@@ -6,23 +6,28 @@ class WebHeader extends Component {
   render() {
     return (
       <>
-        <nav class="web-container br">
-            <section class="top-bar br flex sb">
-              <div class="top-bar-title br">Sopranos</div>
+        <nav class="web-container">
+            <section class="top-bar flex sb">
+              <div class="top-bar-title">Sopranos</div>
               <div class="exit-container br flex sb">
                 <div class="box">
-                  <img src="" alt=""/>
+                  <img src="../../public/icons/mini.ico" alt="Minimize Image"/>
                 </div>
-                <div class="box"></div>
+                <div class="box">
+                  <img src="../../public/icons/maxi.ico" alt="Maximize Image"/>
+                </div>
+                <div class="box">
+                  <img src="exit.png" alt="Exit Image"/>
+                </div>
               </div>
             </section>
-            <section class="nav-bar br">
-              <div class="sec-one br">
-                <box>                 
+            <section class="nav-bar flex br">
+              <div class="sec  flex">
+                <box class="">                 
                 </box>
-                <box>                 
+                <box class="">                 
                 </box>
-                <box>                 
+                <box class="">                 
                 </box>
               </div>
               <div class="sec-two br">
